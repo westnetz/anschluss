214dded254dc5aa732cbb824e5797ba200965cd0
Christian Franke <nobody@nowhere.ws>
2019-03-21 18:49:23 +0100

    Add django.middleware.locale.LocaleMiddleware to MIDDLEWARE

--- project/settings.py
+++ project/settings.py
@@ -20,6 +20,7 @@ class Common(Configuration):
     MIDDLEWARE = [
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.locale.LocaleMiddleware",
         "django.middleware.common.CommonMiddleware",
         "django.middleware.csrf.CsrfViewMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
