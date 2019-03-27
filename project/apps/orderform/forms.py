7bf5a1b097575e8e7fb77d941e3248cdb29a2bf6
Christian Franke <nobody@nowhere.ws>
2019-03-27 23:25:44 +0100

    Add a link to the mail to prefill the form with previous data
    It would be nice for our customers to be able to prefill the form
    again with the data they entered previously if they need to make
    any modifications, so that they do not have to re-enter everything.

--- project/apps/orderform/forms.py
+++ project/apps/orderform/forms.py
@@ -2,8 +2,10 @@ from email.mime.text import MIMEText
 
 from django.conf import settings
 from django.core.mail import EmailMessage
+from django.core.signing import dumps
 from django.forms import Form, CharField, EmailField
 from django.template import loader
+from django.urls import reverse
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
@@ -13,6 +15,10 @@ import yaml
 class OrderForm(Form):
     product_name = CharField(label=_("Product Name"))
     main_email = EmailField(label=_("E-Mail Address"))
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super().__init__(*args, **kwargs)
 
     @property
     def yaml_filename(self):
@@ -32,8 +38,15 @@ class OrderForm(Form):
         return [self.yaml]
 
     @property
+    def redo_url(self):
+        url = reverse("order_prefilled", args=(dumps(self.data),))
+        if self.request:
+            url = self.request.build_absolute_uri(url)
+        return url
+
+    @property
     def context(self):
-        return {"data": self.cleaned_data}
+        return {"data": self.cleaned_data, "redo_url": self.redo_url}
 
     @property
     def body(self):
--- project/apps/orderform/urls.py
+++ project/apps/orderform/urls.py
@@ -4,5 +4,6 @@ from .views import OrderCompleteView, OrderView
 
 urlpatterns = [
     path("", OrderView.as_view(), name="order"),
+    path("prefill/<data>/", OrderView.as_view(), name="order_prefilled"),
     path("success/", OrderCompleteView.as_view(), name="order_complete"),
 ]
